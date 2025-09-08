1f4722bc84118db7ae48198b77be39ef5b282c14
wejdan saad <wejdanal213@gmail.com>
2025-09-09 01:47:32 +0300

    firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,44 +1,56 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("org.jetbrains.kotlin.android")
+    // يجب أن يكون Flutter plugin بعد Android و Kotlin
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+   
 }
+ dependencies {
+  // Import the Firebase BoM
+  implementation(platform("com.google.firebase:firebase-bom:34.2.0"))
 
+
+  // TODO: Add the dependencies for Firebase products you want to use
+  // When using the BoM, don't specify versions in Firebase dependencies
+  // https://firebase.google.com/docs/android/setup#available-libraries
+}
 android {
     namespace = "com.example.billwise_499"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
+    // ثبّت الإصدارات عشان تتطابق البيئات
+    compileSdk = 34
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.billwise_499"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        targetSdk = 34
+
+        // ثبّت أرقام النسخة (عدّلوها وقت الحاجة وادفعوها للـ Git)
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    // استخدم Java 17 (AGP 8.x يتطلبه)
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // مبدئياً نوقّع بمفاتيح debug لسهولة التشغيل
             signingConfig = signingConfigs.getByName("debug")
+            // بإمكانكم إضافة proguard لاحقاً
+            isMinifyEnabled = false
         }
     }
 }
 
 flutter {
+    // مسار مشروع Flutter
     source = "../.."
 }
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,5 @@
+// android/settings.gradle.kts
+
 pluginManagement {
     val flutterSdkPath =
         run {
@@ -11,16 +13,27 @@ pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.9.1" apply false
+    id("com.android.application") version "8.4.2" apply false
+    // يفضّل آخر نسخة مستقرة من Google Services
+    id("com.google.gms.google-services") version "4.4.2" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
+rootProject.name = "billwise_499"
 include(":app")
